7969e5b9ab91dda50d9be88c457f0424fbf6742a
James Morgan <james.morgan.e@googlemail.com>
2016-11-27 15:38:00 +0000

    Removing error messages

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,8 @@ var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var helpers = require('./webpack-helpers');
 
 module.exports = {
+	target: 'node-webkit',
+
 	entry: {
 		'polyfills': './browser-content/polyfills.ts',
 		'vendor': './browser-content/vendor.ts',
@@ -59,66 +61,9 @@ module.exports = {
 		new webpack.optimize.CommonsChunkPlugin({
 			name: ['app', 'vendor', 'polyfills']
 		}),
-
 		new HtmlWebpackPlugin({
 			template: 'browser-content/index.html'
 		}),
-
 		new ExtractTextPlugin('[name].css')
-
-	],
-
-	target: 'node-webkit'
+	]
 };
-
-// var path = require('path');
-// var webpack = require('webpack');
-// var CommonsChunkPlugin = webpack.optimize.CommonsChunkPlugin;
-//
-// module.exports = {
-// 	devtool: 'inline-source-map',
-// 	// devtool: 'source-map',
-// 	debug: true,
-//
-// 	entry: {
-// 		'vendor': './browser-content/vendor',
-// 		'app': './browser-content/main'
-// 	},
-//
-// 	output: {
-// 		path: __dirname + '/browser-content-build/',
-// 		publicPath: 'browser-content-build/',
-// 		filename: '[name].js',
-// 		sourceMapFilename: '[name].js.map',
-// 		chunkFilename: '[id].chunk.js'
-// 	},
-//
-// 	resolve: {
-// 		// The empty string is needed for pulling in Node modules which do not need to provide an extension
-// 		extensions: ['', '.ts', '.js', '.json', '.css', '.html']
-// 	},
-//
-// 	module: {
-// 		loaders: [
-// 			// {
-// 			// 	test: /\.ts$/,
-// 			// 	exclude: [/node_modules/],
-// 			// 	loader: 'ts',
-// 			// },
-// 			{
-// 				test: /\.ts$/,
-// 				loaders: 'ts'
-// 			},
-// 			{
-// 				test: /\.css$/,
-// 				loaders: 'style!css'
-// 			}
-// 		]
-// 	},
-//
-// 	plugins: [
-// 		new CommonsChunkPlugin({name: 'vendor', filename: 'vendor.js', minChunks: Infinity}),
-// 		new CommonsChunkPlugin({name: 'common', filename: 'common.js'})
-// 	],
-// 	target: 'node-webkit'
-// };
